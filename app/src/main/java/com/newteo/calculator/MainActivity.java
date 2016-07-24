21a3c38e89b64ff872e0812651c246269bddfefd
joephon <306519779@qq.com>
2016-07-25 01:48:27 +0800

    made a lot

--- app/src/main/java/com/newteo/calculator/MainActivity.java
+++ app/src/main/java/com/newteo/calculator/MainActivity.java
@@ -1,13 +1,169 @@
 package com.newteo.calculator;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+    Button btn6;
+    Button btn7;
+    Button btn8;
+    Button btn9;
+    Button btn0;
+    Button btn_plus;
+    Button btn_minus;
+    Button btn_multiply;
+    Button btn_divide;
+    Button btn_equal;
+    Button btn_clear;
+    Button btn_delete;
+    Button btn_point;
+    EditText display;
+    String count;
+    boolean hasPoint = false;
+    int feature;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = (EditText) findViewById(R.id.display);
+        btn0 = (Button) findViewById(R.id.btn0);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btn_plus = (Button) findViewById(R.id.btn_plus);
+        btn_minus = (Button) findViewById(R.id.btn_minus);
+        btn_multiply = (Button) findViewById(R.id.btn_multiply);
+        btn_divide = (Button) findViewById(R.id.btn_divide);
+        btn_clear = (Button) findViewById(R.id.btn_clear);
+        btn_delete = (Button) findViewById(R.id.btn_delete);
+        btn_equal = (Button) findViewById(R.id.btn_equal);
+        btn_point = (Button) findViewById(R.id.btn_point);
+
+
+        btn0.setOnClickListener(this);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btn_plus.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_divide.setOnClickListener(this);
+        btn_equal.setOnClickListener(this);
+        btn_clear.setOnClickListener(this);
+        btn_delete.setOnClickListener(this);
+        btn_point.setOnClickListener(this);
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        String input = ((Button) v).getText().toString();
+        String value = display.getText().toString();
+
+        switch (v.getId()) {
+            case R.id.btn0:
+            case R.id.btn1:
+            case R.id.btn2:
+            case R.id.btn3:
+            case R.id.btn4:
+            case R.id.btn5:
+            case R.id.btn6:
+            case R.id.btn7:
+            case R.id.btn8:
+            case R.id.btn9:
+                handleInput(input, value);
+                break;
+            case R.id.btn_point:
+                handlePoint(input, value);
+                break;
+            case R.id.btn_plus:
+            case R.id.btn_minus:
+            case R.id.btn_multiply:
+            case R.id.btn_divide:
+                break;
+            case R.id.btn_delete:
+                handleDelete(value);
+                break;
+            case R.id.btn_clear:
+                hasPoint = false;
+                display.setText(null);
+                break;
+            case R.id.btn_equal:
+                getResult();
+                break;
+        }
+
+
+    }
+
+    private void handleInput(String input, String value) {
+        if (value.length() == 1) {
+            if (value.equals("0"))
+                return;
+        }
+        else
+            display.setText(value + input);
+
+    }
+
+
+    private void handlePoint(String input, String value) {
+        if (!hasPoint) {
+            display.setText(value + input);
+            hasPoint = true;
+        }
+
+    }
+
+    private void handleDelete(String value) {
+        if (value != null && !value.equals("")) {
+            value = value.substring(0, value.length() - 1);
+            display.setText(value);
+            if (value.contains("."))
+                hasPoint = true;
+            else
+                hasPoint = false;
+        }
+    }
+
+    private void getResult() {
+
+    }
+
 }
